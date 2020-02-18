e2525de32c07700edb79c4cacbe253d7e8a5cb04
seanantosiak <seanantosiak@gmail.com>
2020-02-18 15:55:04 -0500

    updated from local repo

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,22 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    next = []
+    for x in ancestors:
+        if x[1] == starting_node:
+            next.append(x)
+    if next == []:
+        return(-1)
+    else:
+        ances = []
+        while len(next) > 0:
+            new_next = []
+            for x in next:
+                for y in ancestors:
+                    if x[0] == y[1]:
+                        new_next.append(y)
+            if new_next:
+                next = new_next
+            else:
+                for x in next:
+                    ances.append(x[0])
+                return(min(ances))
